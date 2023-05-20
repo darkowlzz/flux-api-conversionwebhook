f58e5f6db854c6515cacefb9ab16578142548f7c
Sunny <darkowlzz@protonmail.com>
2023-05-20 15:25:08 +0000

    Enable conversion webhook
    Signed-off-by: Sunny <darkowlzz@protonmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -90,6 +90,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&sourcev1.GitRepository{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "GitRepository")
+		os.Exit(1)
+	}
+	if err = (&sourcev1beta2.GitRepository{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "GitRepository")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
